0e43847d5437a34cf3ab839325752e1c37f1a17f
tomcusack1 <tom@tom-cusack.com>
2019-10-13 22:49:08 +0100

    Refactors example code

--- example.py
+++ example.py
@@ -28,83 +28,61 @@ def mine():
     previous_hash = blockchain.hash(last_block)
     block = blockchain.new_block(proof, previous_hash)
 
-    response = {
-        'message': "New Block Forged",
+    return jsonify({
+        'message': 'New Block Forged',
         'index': block['index'],
         'transactions': block['transactions'],
         'proof': block['proof'],
         'previous_hash': block['previous_hash'],
-    }
-    return jsonify(response), 200
+    }), 200
 
 
 @app.route('/transactions/new', methods=['POST'])
 def new_transaction():
+    """Creates a new transaction."""
     values = request.get_json()
 
-    # Check that the required fields are in the POST'ed data
-    required = ['sender', 'recipient', 'amount']
-    if not all(k in values for k in required):
+    if not all(k in values for k in ['sender', 'recipient', 'amount']):
         return 'Missing values', 400
 
-    # Create a new Transaction
     index = blockchain.new_transaction(values['sender'], values['recipient'], values['amount'])
 
-    response = {'message': f'Transaction will be added to Block {index}'}
-    return jsonify(response), 201
+    return jsonify({'message': f'Transaction will be added to Block {index}'}), 201
 
 
 @app.route('/chain', methods=['GET'])
 def full_chain():
-    response = {
-        'chain': blockchain.chain,
-        'length': len(blockchain.chain),
-    }
-    return jsonify(response), 200
+    """Returns the whole blockchain."""
+
+    return jsonify({'chain': blockchain.chain, 'length': len(blockchain.chain)}), 200
 
 
 @app.route('/nodes/register', methods=['POST'])
 def register_nodes():
+    """Registers a new node to the network."""
     values = request.get_json()
-
     nodes = values.get('nodes')
+
     if nodes is None:
         return "Error: Please supply a valid list of nodes", 400
 
     for node in nodes:
         blockchain.register_node(node)
 
-    response = {
+    return jsonify({
         'message': 'New nodes have been added',
         'total_nodes': list(blockchain.nodes),
-    }
-    return jsonify(response), 201
+    }), 201
 
 
 @app.route('/nodes/resolve', methods=['GET'])
 def consensus():
-    replaced = blockchain.resolve_conflicts()
-
-    if replaced:
-        response = {
-            'message': 'Our chain was replaced',
-            'new_chain': blockchain.chain
-        }
+    """Resolves a chain by requesting consensus on the network."""
+    if blockchain.resolve_conflicts():
+        return jsonify({'message': 'Our chain was replaced', 'new_chain': blockchain.chain}), 200
     else:
-        response = {
-            'message': 'Our chain is authoritative',
-            'chain': blockchain.chain
-        }
-
-    return jsonify(response), 200
+        return jsonify({'message': 'Our chain is authoritative', 'new_chain': blockchain.chain}), 200
 
 
 if __name__ == '__main__':
-    from argparse import ArgumentParser
-
-    parser = ArgumentParser()
-    parser.add_argument('-p', '--port', default=5000, type=int, help='port to listen on')
-    args = parser.parse_args()
-    port = args.port
-
-    app.run(host='0.0.0.0', port=port)
+    app.run(host='0.0.0.0', port=5000)
